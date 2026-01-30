bf9bcdc92e830994c3006c23041d92184627ea55
Abhishek <abhishekpant.as@gmail.com>
2026-01-30 21:19:12 +0530

    3. Autologging and tracking

---
+++ src/autolog.py
@@ -0,0 +1,75 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import mlflow
+import dagshub
+
+
+dagshub.init(repo_owner='Rusty-user365', repo_name='MLOPS_MLFLOW_TUTORIAL', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/Rusty-user365/MLOPS_MLFLOW_TUTORIAL.mlflow")
+
+
+
+#load wine dataset
+
+wine=load_wine()
+
+X=wine.data
+y=wine.target
+
+#Train test split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.10,random_state=42)
+
+
+#define the params for RF Model
+
+max_depth=5
+n_estimators=5
+
+mlflow.autolog()
+# write your experiments here
+mlflow.set_experiment("Wine_Quality_Classification_Autolog")
+
+with mlflow.start_run():
+    #Create RF Model
+    rf=RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators,random_state=42)
+    
+    #Fit the model
+    rf.fit(X_train,y_train)
+    
+    #Predict the model
+    y_pred=rf.predict(X_test)
+    
+    #Calculate accuracy
+    acc=accuracy_score(y_test,y_pred)
+ 
+   
+
+
+    # creating a confusuion matrix plot  ctrl+/
+    cm=confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(8,6))
+    sns.heatmap(cm,annot=True,fmt='d',cmap='Blues',xticklabels=wine.target_names,yticklabels=wine.target_names )
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')   
+    plt.savefig('confusion_matrix.png')
+   
+    mlflow.log_artifact(__file__)
+
+
+    # tags
+    mlflow.set_tag("Author", "Abhishek")
+
+    mlflow.set_tag("Model", "RandomForestClassifier")
+    mlflow.set_tag("Dataset", "Wine Dataset")
+
+  
+    print(acc)
